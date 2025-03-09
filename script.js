743ad9515f86a4b7c9665fbdfdd3494abdddf861
Dusciuc Ioan Daniel <dusciuc_ioan@yahoo.com>
2025-03-09 01:14:27 +0000

    9 Mar 2025, 03:14

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let stat = false;
 function like(id)
 {
     const likeButton = document.getElementById(id);
-    if(!stat) stat = true;
-    if(stat) stat = false;
+    if(!stat) 
+    {
+        stat = true;
+        likeButton.innerText = "❤️";
+    }
+    if(stat)
+    {    
+        stat = false;
+        likeButton.innerText = "♡";
+    }
 }
