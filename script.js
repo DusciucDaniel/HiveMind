aa8a7c660ab3fed118afb004290c13fccdb075e3
Dusciuc Ioan Daniel <dusciuc_ioan@yahoo.com>
2025-03-28 21:41:23 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function like(id)
-{
-    console.log("sad");
-    if(document.getElementById(id).innerHTML === "♡") {
-        document.getElementById(id).innerHTML = "❤️";
-    } else {
-        document.getElementById(id).innerHTML = "♡";
-    }
-}
